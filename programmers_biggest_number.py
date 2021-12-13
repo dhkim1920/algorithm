f8ce6407b17b37740bf793d606dfc719d173f9b7
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2021-12-13 20:56:55 +0900

    가장 큰 수 - python 문자열 연산의 이해가 필요 - lambda를 쓰면 더 편함

--- programmers_biggest_number.py
+++ programmers_biggest_number.py
@@ -1,24 +1,13 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42746
 
-
 def solution(numbers):
-    longest_len = len(str(max(numbers)))
+    numbers_str = sorted(list(map(lambda x: str(x) * 3, numbers)), reverse=True)
+    result = list(map(lambda x: x[:len(x)//3], numbers_str))
 
-    for i in range(len(numbers) - 1, 0, -1):
-        for j in range(i):
-            a = numbers[j] if len(str(numbers[j])) >= longest_len else numbers[j] * 10
-            b = numbers[j + 1] if len(str(numbers[j + 1])) >= longest_len else numbers[j + 1] * 10
-
-            if a < b:
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
-            elif a == b:
-                if numbers[j] > numbers[j + 1]:
-                    numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
-
-
-    return ''.join(map(str, numbers))
+    return str(int(''.join(result)))
 
 
 # print(solution([0, 0, 0]))
 # print(solution([3, 30, 34, 5, 9]))
-print(solution([3030, 303]))
+# print(solution([3030, 303]))
+print(solution([998, 9]))
