d671f1618d8d9a7a67199bef15d7e461667cdb25
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2021-12-15 22:54:59 +0900

    프로그래머스 h index - 0, 1일 경우 바로 return 처리

--- programmers_h_index_problem.py
+++ programmers_h_index_problem.py
@@ -1,16 +1,27 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42747
 
 def solution(citations):
-    answer = 0
-
-    if sum(citations) == 0:
-        return answer
+    sum_citations = sum(citations)
+    if sum_citations == 0:
+        return 0
+    elif sum_citations == 1:
+        return 1
     else:
-        print(sorted(citations))
+        answer = 1
+        citations = sorted(citations)
+
+        for i in range(len(citations)):
+            if citations[i] == 0:
+                continue
+            else:
+                print(citations[i])
+
+        print("--------------------")
+
         return answer
 
 
 print(solution([3, 0, 6, 1, 5]))
 # print(solution([1, 1, 5, 6]))
 # print(solution([0]))
-# print(solution([0, 0, 0]))
+# print(solution([0, 0, 0, 1]))
