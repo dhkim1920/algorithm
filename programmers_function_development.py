07a657029a77c1819feed154a26067433fe97090
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2021-12-19 20:29:04 +0900

    프로그래머스 기능개발 - 문제 풀이

--- programmers_function_development.py
+++ programmers_function_development.py
@@ -2,7 +2,33 @@
 
 def solution(progresses, speeds):
     answer = []
+
+    progresses = list(reversed(progresses))
+    speeds = list(reversed(speeds))
+
+    while True:
+        if len(progresses) == 0:
+            break
+
+        for i in range(len(progresses)):
+            progresses[i] = progresses[i] + speeds[i]
+
+        progresses_len = len(progresses)
+        while True:
+            try:
+                if progresses[-1] >= 100:
+                    progresses.pop()
+                else:
+                    break
+            except IndexError as e:
+                break
+
+        if progresses_len - len(progresses) > 0:
+            answer.append(progresses_len - len(progresses))
+
     return answer
 
 
-print(solution([93, 30, 55], [1, 30, 5]))
+# print(solution([93, 30, 55], [1, 30, 5]))
+# print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
+print(solution([0, 1], [99, 99]))
