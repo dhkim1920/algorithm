1406a2437eeaf8961e47fff9aaff0ef83ec9f419
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2021-12-27 19:34:03 +0900

    프로그래머스 프린터 - stack deque로 변경하여 해결

--- programmers_printer.py
+++ programmers_printer.py
@@ -1,40 +1,21 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42587
+from collections import deque
+
 
 def solution(priorities, location):
     answer = []
-    tmp_stack = []
 
-    priorities = [[x, i] for i, x in enumerate(priorities)]
-    print(priorities)
-    tmp = (priorities[location])
-    print(tmp)
+    values = {x: priorities[x] for x in range(0, len(priorities))}
+    indices = deque([i for i in range(0, len(priorities))])
 
-    while len(priorities) > 0:
-        m = max(priorities)
-        if priorities[-1] != m:
-            tmp_stack.append(priorities.pop())
-            print("after not max pop: " + str(priorities))
-            print("--------------------------")
+    while indices:
+        if values[indices[0]] < max(priorities):
+            indices.append(indices.popleft())
         else:
-            v = priorities.pop()
-            print("else max pop: " + str(priorities))
-            answer.append(v)
-            if len(priorities) > 0:
-                t = priorities[-1] == max(priorities)
-            while len(tmp_stack) > 0:
-                # print("while : " + str(priorities) + " , top: " + str(priorities[-1]) + ", max: " + str(m))
-                if t:
-                    answer.append(tmp_stack.pop())
-                    print("else append tmp_stack to answer: " + str(answer))
-                else:
-                    priorities.append(tmp_stack.pop())
-                    print("else append tmp_stack to priorities: " + str(priorities))
-            print("--------------------------")
-
-    print(answer)
-    return answer.index(tmp) + 1
+            answer.append(indices[0])
+            priorities.remove(values[indices.popleft()])
+    return answer.index(location) + 1
 
 
-
-# print(solution([2, 1, 3, 2], 2))
-print(solution([1, 1, 9, 1, 1, 1], 0))
+print(solution([2, 1, 3, 2], 2))
+# print(solution([1, 1, 9, 1, 1, 1], 0))
