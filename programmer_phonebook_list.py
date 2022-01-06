18f8cdc3df7ef4c3d46ebbca6845b5ba34c0a989
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2022-01-06 20:13:14 +0900

    프로그래머스 전화번호 목록 - 효율설 통과 못한 버전

--- programmer_phonebook_list.py
+++ programmer_phonebook_list.py
@@ -4,12 +4,16 @@
 def solution(phone_book):
     answer = True
 
-    phone_book = {k: 0 for k in phone_book}
-
-    for i in phone_book.keys():
-        for j in phone_book.keys():
-            if i in j:
-                if i is not j:
+    phone_book_len = len(phone_book)
+    for i in range(0, phone_book_len):
+        for j in range(i + 1, phone_book_len):
+            i_len = len(phone_book[i])
+            j_len = len(phone_book[j])
+            if i_len > j_len:
+                if phone_book[i][:j_len] == phone_book[j]:
+                    return False
+            else:
+                if phone_book[j][:i_len] == phone_book[i]:
                     return False
 
     return answer
@@ -19,4 +23,4 @@ print(solution(["119", "97674223", "1195524421"]))
 print(solution(["123", "456", "789"]))
 print(solution(["12", "123", "1235", "567", "88"]))
 print(solution(["11", "22", "111"]))
-print(solution(["111", "22", "11"]))
+print(solution(["987987", "87"]))
