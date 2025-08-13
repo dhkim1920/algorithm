3b0348fa9b98bb4fc3950e47c8c7d0887f4a4c8a
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2025-08-13 21:08:39 +0900

    - boj 9655 dp문제 해결

--- boj_9655.py
+++ boj_9655.py
@@ -1,15 +1,22 @@
 # https://www.acmicpc.net/problem/9655
 # 이 문제는 순서가 정해져있고 1, 3으로 N이 차감되므로 홀수에는 무조건 이긴다.
+# N = int(input())
+# print("SK" if N % 2 else "CY")
 # 그러나 DP 연습을 위해 점화식을 이용해서 풀어보자
 
 N = int(input())
-dp = [False] * N
+dp = [False] * (N + 1)
+
 
 def solution(n):
+    dp[1] = True
+    if N >= 3:
+        dp[3] = True
 
-    for i in range(4, N):
-        dp[i] = (n >= 1 and not dp[i - 1]) or (n >= 3 and not dp[i - 3])
+    for i in range(4, n + 1):
+        dp[i] = not dp[i - 1] or not dp[i - 3]
 
-    return dp[n - 1]
+    return "SK" if dp[n] else "CY"
+
 
 print(solution(N))
