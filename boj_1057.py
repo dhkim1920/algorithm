0005f1bedcca884851577658b96e89ee9d39e331
DESKTOP-PVJ8BUN\d <dhkim1920@gmail.com>
2025-08-16 23:54:38 +0900

    - boj 1057 수학 해결

--- boj_1057.py
+++ boj_1057.py
@@ -1,2 +1,15 @@
 # https://www.acmicpc.net/problem/1057
 
+N, JM, HS = map(int, input().split())
+
+
+def solution(x, y):
+    answer = 0
+    while x != y:
+        x = (x + 1) // 2
+        y = (y + 1) // 2
+        answer += 1
+    return answer
+
+
+print(solution(JM, HS))
